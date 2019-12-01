a1e7a8c9bab8bf2a9a4ac352e6998dd6f0164765
Benjamin Spriggs <ben@sprico.com>
2019-11-30 17:27:29 -0800

    Add interface

--- web/src/book.ts
+++ web/src/book.ts
@@ -1,3 +1,10 @@
-export function fetchBook(id: string) {
+export interface Book {
+  id: string;
+  displayName: string;
+  author: string;
+  readings: any[];
+}
+
+export function fetchBook(id: string): Promise<Book> {
   return fetch(`http://localhost:5041/books/${id}`).then(resp => resp.json());
 }
