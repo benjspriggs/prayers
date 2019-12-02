ceb3481bef865c776526ad7e8290c598b377d30f
Benjamin Spriggs <ben@sprico.com>
2019-12-01 22:53:51 -0800

    Adjust parsing and formatting for one text

--- scripts/parser.py
+++ scripts/parser.py
@@ -3,33 +3,36 @@ Parses an HTML document provided by the https://bahai.org website.
 """
 import sys
 from lxml import html
+import json
 
-def _(s):
-    return s.strip()
+def parse(source: str):
+    """
+    Parses the Prayers and Meditations by Baha'u'llah, downloaded from the
+    Baha'i Reference Library.
 
-def _p(prayers):
-    for prayer_string in prayers:
-        if prayer_string:
-            yield prayer_string
+    https://www.bahai.org/library/authoritative-texts/bahaullah/prayers-meditations/
+    """
+    tree = html.parse(source)
 
-def parse(source):
-    tree = html.fromstring(source)
+    def _t(el, selector):
+        return [line.strip().replace('\r\n                        ', ' ') for line in el.xpath(selector)]
 
-    def _t(selector):
-        return [line.strip().replace('\r\n                        ', ' ') for line in tree.xpath(selector)]
+    body = tree.xpath('//*[@class="library-document-content"]')[0]
+    title = _t(body, '//h1[@class="brl-title"]//text()')
+    author = _t(body, '//p[@class="brl-subtitle"]/text()')
+    notes = _t(body, '//p[@class="brl-doc-byline"]/text()')
+    sections = body.xpath('//*[@class="brl-btmmargin"]')
 
-    title = _t('//h1[@class="brl-title"]//text()')
-    author = _t('//p[@class="brl-subtitle"]/text()')
-    notes = _t('//p[@class="brl-doc-byline"]/text()')
-    prayers = _t('//p[0 or 1 or 2]/text()')
-
-    print(list({
+    return ({
         'author': author,
-        'title': title,
+        'book_title': title,
         'notes': notes,
-        'prayer': p
-    } for p in _p(prayers)))
+        'sections': [{
+            'title': _t(section, '//p[contains(@class, "brl-global-selection-number")]/text()'),
+            'body': _t(section, '//span[@class="brl-uppercase"]/text()|//p[position() > 1]/text()'),
+        } for section in sections]
+    })
 
 if __name__ == "__main__":
-    source = sys.stdin.buffer.read() 
-    parse(source)
+    parsed = parse(sys.argv[1])
+    print(json.dumps(parsed))
