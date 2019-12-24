b446e94401987deda5e32a0e364db4cb95358202
Benjamin Spriggs <ben@sprico.com>
2019-12-24 13:52:27 -0800

    Handle native errors instead of continuing

--- scripts/generate_placeholders.ps1
+++ scripts/generate_placeholders.ps1
@@ -5,10 +5,27 @@ This file genrates data to be viewed in readings/poc.html. Run this from ./scrip
 
 $ErrorActionPreference = 'Stop';
 
-py .\bahaullah_prayers.py "C:\Users\bspriggs\Downloads\Prayers and Meditations by Bahá’u’lláh.xht";
-py .\general_prayers.py "C:\Users\bspriggs\Downloads\Bahá’í Prayers.xhtml";
-cd ../server;
-npm run import:general;
-npm run import:bahaullah;
-cd ..;
-cp server/*.json ./web/static/assets;
+py .\bahaullah_prayers.py "./bahaullah.xht";
+
+if (-not $?) {
+    throw 'Error'
+}
+
+py .\general_prayers.py "./general_prayers.xhtml";
+
+if (-not $?) {
+    throw 'Error'
+}
+
+npm run --prefix ../server import:general;
+
+if (-not $?) {
+    throw 'Error'
+}
+
+npm run --prefix ../server import:bahaullah;
+if (-not $?) {
+    throw 'Error'
+}
+
+cp ../server/*.json ../web/static/assets;
