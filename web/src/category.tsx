420a97da3f1794d21fc184582e3434ccac5c0e16
Benjamin Spriggs <ben@sprico.com>
2020-01-05 12:47:18 -0800

    Render a parent > resource path for categories

--- web/src/category.tsx
+++ web/src/category.tsx
@@ -4,10 +4,39 @@ import { useDatabase } from "./lib/db.js";
 
 export const db = useDatabase<Category>({ name: "categories" });
 
-export function fetchCategory(id: string) {
+export function fetchCategory(
+  id: string
+): Promise<PouchDB.Core.ExistingDocument<Category>> {
   return db.then(({ localDb }) => localDb.get(id));
 }
 
-export function renderCategory(data: Category) {
-  return <p>I am a placeholder</p>;
+export function fetchCategories() {
+  return db
+    .then(({ localDb }) => localDb.allDocs({ include_docs: true }))
+    .then(response => response.rows.map(row => row.doc!));
+}
+
+export function renderCategory(data?: PouchDB.Core.ExistingDocument<Category>) {
+  if (!data) {
+    return;
+  }
+
+  console.log(data);
+
+  const parents = data.parent
+    ? Promise.all(data.parent.map(fetchCategory)).then(categories =>
+        categories.map(category => (
+          <li slot="parent">
+            <category-link data-category-id={category._id}></category-link>
+          </li>
+        ))
+      )
+    : null;
+
+  return (
+    <category-summary data-category-id={data._id}>
+      <h1 slot="title">{data.displayName}</h1>
+      {parents ? parents : <p>This category has no parents.</p>}
+    </category-summary>
+  );
 }
