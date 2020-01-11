13730964b1def5fb59f87156a861cdc7eeea2747
Benjamin Spriggs <ben@sprico.com>
2020-01-11 13:29:48 -0800

    Add tests

--- web/src/render.spec.ts
+++ web/src/render.spec.ts
@@ -1,3 +1,33 @@
 import "jest";
 
-test.todo("render");
+import { render } from "./render";
+
+describe("createElement", () => {
+  const { createElement } = render;
+
+  it("creates elements", () => {
+    const h1 = document.createElement("h1");
+    expect(createElement("h1", {})).toEqual(h1);
+  });
+
+  it("sets props", () => {
+    const h1 = document.createElement("h1");
+    h1.setAttribute("data-prop", "2");
+
+    expect(
+      createElement("h1", {
+        "data-prop": "2"
+      })
+    ).toEqual(h1);
+  });
+
+  it("sets classes from `className`", () => {});
+
+  describe("children", () => {
+    it.todo("handles arrays");
+    it.todo("handles null");
+    it.todo("handles undefined");
+    it.todo("handles promises");
+    it.todo("handles raw strings");
+  });
+});
