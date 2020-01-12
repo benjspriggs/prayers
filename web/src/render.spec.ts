786dc8c30fb05c4d2ba49b9675453cd424a8fc8d
Benjamin Spriggs <ben@sprico.com>
2020-01-11 16:36:32 -0800

    Add test for null and undefined props

--- web/src/render.spec.ts
+++ web/src/render.spec.ts
@@ -15,28 +15,50 @@ describe("createElement", () => {
     expect(createElement("h1", {})).toEqual(h1);
   });
 
-  it("sets props", () => {
-    const h1 = document.createElement("h1");
-    h1.setAttribute("data-prop", "2");
+  describe("props", () => {
+    it("sets props", () => {
+      const h1 = document.createElement("h1");
+      h1.setAttribute("data-prop", "2");
 
-    expect(
-      createElement("h1", {
-        "data-prop": "2"
-      })
-    ).toEqual(h1);
-  });
+      expect(
+        createElement("h1", {
+          "data-prop": "2"
+        })
+      ).toEqual(h1);
+    });
 
-  it("sets classes from `className`", () => {
-    const h1 = document.createElement("h1");
-    h1.classList.add("foo");
-    h1.classList.add("bar");
-    h1.classList.add("baz");
+    it("sets classes from `className`", () => {
+      const h1 = document.createElement("h1");
+      h1.classList.add("foo");
+      h1.classList.add("bar");
+      h1.classList.add("baz");
 
-    expect(
-      createElement("h1", {
-        className: "foo bar baz"
-      })
-    ).toEqual(h1);
+      expect(
+        createElement("h1", {
+          className: "foo bar baz"
+        })
+      ).toEqual(h1);
+    });
+
+    it("ignores null props", () => {
+      const h1 = document.createElement("h1");
+
+      expect(
+        createElement("h1", {
+          "data-my-prop": null
+        })
+      ).toEqual(h1);
+    });
+
+    it("ignores undefined props", () => {
+      const h1 = document.createElement("h1");
+
+      expect(
+        createElement("h1", {
+          "data-my-prop": undefined
+        })
+      ).toEqual(h1);
+    });
   });
 
   describe("children", () => {
